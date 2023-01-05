0274559a9e4f24d6017eb786c2533fa5442f6aa6
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2023-01-05 00:37:15 -0400

    Se colocan nuevas cosas

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const board = document.getElementById("board");
 const scoreBoard = document.getElementById("scoreBoard");
 const startButton = document.getElementById("start");
 const gameOverSign = document.getElementById("gameOver");
+const buttonBackGame = document.querySelector('.backGame')
+const buttonRetry = document.querySelector('.retry');
+const buttonExit = document.querySelector('.exit');
 //game settings
 const boardSize = 10;
-const gameSpeed = 300;
+let gameSpeed;
 const squareTypes = {
     emptySquare: 0,
     snakeSquare: 1,
-    foodSquare: 2
+    foodSquare: 2,
+    headSnake:3
 };
 const directions = {
     ArrowUp:-10,
@@ -20,14 +24,18 @@ const directions = {
 //Game Vaiables
 let snake;
 let score;
+var varScore;
+let varTime = 50;
 let direction;
 let boardSquares;
 let emptySquares;
 let moveInterval;
 
 const drawSnake=()=>{
-    snake.forEach(square => drawSquare(square, 'snakeSquare'));
-}
+    snake.forEach(square =>{
+        drawSquare(square, 'snakeSquare');
+    });
+} 
 
 const drawSquare = (square,type)=>{
     const [ row, column ] = square.split('');
@@ -48,7 +56,7 @@ const moveSnake = ()=>{
             Number(snake[snake.length-1])+directions[direction])
             .padStart(2,'0');
     const [ row,column] = newSquare.split('');
-
+        console.log(newSquare);
     if(newSquare < 0 || 
         newSquare > boardSize * boardSize || 
         (direction === 'ArrowRight' && column == 0) ||
@@ -64,19 +72,34 @@ const moveSnake = ()=>{
             drawSquare(emptySquare, 'emptySquare');
         }drawSnake(); 
     }
-
 }
 
 const addFood=()=>{
     score++;
+    console.log(varScore);
+    console.log(varTime);
     updateScore();
     createRandomFood();
+    console.log("comiste una manzana");
+    if(score == varScore){
+        gameSpeed-=varTime;
+        console.log("cambia velocidad");
+        clearInterval(moveInterval);
+        moveInterval = setInterval(()=>moveSnake(),gameSpeed);
+        varScore+=10;
+        console.log('va a ua velocidad de: '+gameSpeed);
+    }
+    
 }
 
 const gameOver =()=>{
+    console.log(gameSpeed);
     gameOverSign.style.display = 'block';
     clearInterval(moveInterval);
     startButton.disabled = false;
+    gameOverSign.addEventListener('click',()=>{
+        gameOverSign.style.display = 'none';
+    });
 }
 
 const setDirection = (newDirection)=> {
@@ -86,7 +109,9 @@ const setDirection = (newDirection)=> {
 const directionEvent =(key)=>{
     switch (key.code) {
         case 'ArrowUp':
-            if(direction != 'ArrowDown') setDirection(key.code)
+            if(direction != 'ArrowDown'){
+                setDirection(key.code)
+            } 
             break;
         case 'ArrowDown':
             if(direction != 'ArrowUp') setDirection(key.code)
@@ -106,7 +131,8 @@ const createRandomFood=()=>{
 }
 
 const updateScore = ()=>{
-    scoreBoard.textContent = score;
+    scoreBoard.textContent = 'Score: '+score;
+ 
 }
 
 const createBoard = ()=>{
@@ -125,6 +151,8 @@ const createBoard = ()=>{
 const setGame=()=>{
     snake = ['00','01','02','03'];
     score = snake.length;
+    gameSpeed = 400;
+    varScore = 10;
     direction = 'ArrowRight';
     boardSquares = Array.from(Array(boardSize),()=> new Array(boardSize).fill(squareTypes.emptySquare))
     console.log(boardSquares);
@@ -132,6 +160,7 @@ const setGame=()=>{
     emptySquares = [];
     createBoard();
 }
+
 
 const startGame=()=>{
     setGame();
@@ -144,4 +173,27 @@ const startGame=()=>{
     moveInterval = setInterval(()=>moveSnake(),gameSpeed);
 }
 
+const backGame=()=>{
+    moveInterval = setInterval(()=>moveSnake(),gameSpeed);
+    document.querySelector('.pause').style.display = 'none';
+}
+
+const retry =()=>{
+    startGame();
+    document.querySelector('.pause').style.display = 'none';
+}
+
 startButton.addEventListener("click",startGame);
+buttonBackGame.addEventListener('click',backGame);
+buttonRetry.addEventListener('click',retry);
+buttonExit.addEventListener('click',()=>{
+    confirm('Seguro');
+});
+
+document.addEventListener("keydown",(e)=>{
+    if(e.key == 'Escape'){
+        e.preventDefault();
+        document.querySelector('.pause').style.display = 'block';
+        clearTimeout(moveInterval);    
+    }
+});
